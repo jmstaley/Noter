0cce3f50d70cf64ced4b62db787fbec3b6488f3a
Jon Staley <jon@spandexbob.co.uk>
2011-10-13 22:56:53 +0100

    start adding sqlalchemy

---
+++ models.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flaskext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)                                                                                                                            
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/sugar.db'
+db = SQLAlchemy(app)
+
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('note_id', db.Integer, db.ForeignKey('note.id'))
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    pw_hash = db.Column(db.String(255))
+    email = db.Column(db.String(120))
+    created_date = db.Column(db.DateTime())
+
+    def __init__(self, username, pw_hash, email, created_date):
+        self.username = username
+        self.pw_hash = pw_hash
+        self.email = email
+        self.created_date = created_date
+
+    def __repr__(self):                                                                                                                          
+        return '<User %s>' % self.username
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(255))
+    html_entry = db.Column(db.Text())
+    raw_entry = db.Column(db.Text())
+    tags = db.relationship('Tag', secondary=tags,
+        backref=db.backref('notes', lazy='dynamic'))
+
+    def __init__(self, uid, title, html_entry, raw_entry):
+        self.uid = uid
+        self.title = title
+        self.html_entry = html_entry
+        self.raw_entry = raw_entry
+
+    def __repr__(self):
+        return '<Note %s>' % self.title
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String(80))
+
+    def __init__(self, value):
+        self.value = value
